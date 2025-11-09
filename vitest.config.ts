76ab3893d8c583cbe0d01ac17b9a31e1cfaa8a92
supakkit <87434100+supakkit@users.noreply.github.com>
2025-11-09 21:32:07 +0700

    chore(config): exclude test files from TS compiling

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,9 @@ export default defineConfig(({ mode }) => ({
     environment: "node",
     setupFiles: ["./src/tests/setup/setup.ts"],
     env: loadEnv(mode, process.cwd(), ""),
-    reporters: ["verbose",],  // shows full details across files
+    reporters: ["verbose"], // shows full details across files
     maxConcurrency: 1, // limits how many test files run in parallel
+    include: ["src/**/*.test.ts"],
+    exclude: ["dist", "node_modules"],
   },
 }));
