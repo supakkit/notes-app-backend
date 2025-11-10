66320f42c9ed2427048804154ff5a8e7a1d33a92
supakkit <87434100+supakkit@users.noreply.github.com>
2025-11-10 14:33:09 +0700

    fix(cors): fix type of frontend url in cors options

--- src/middleware/corsOptions.ts
+++ src/middleware/corsOptions.ts
@@ -2,7 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    process.env.FRONTEND_URL,
+    process.env.FRONTEND_URL || "",
   ],
   credentials: true, // allow cookies to be sent
 };
