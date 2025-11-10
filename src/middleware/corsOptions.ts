7f6fdd9a989a71153fd020042b0069910c0d679b
supakkit <87434100+supakkit@users.noreply.github.com>
2025-11-10 13:41:38 +0700

    fix(cors): fix incorrect allowing url

--- src/middleware/corsOptions.ts
+++ src/middleware/corsOptions.ts
@@ -2,7 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://notes-app-lime-eta.vercel.app/",
+    "https://notes-app-lime-eta.vercel.app",
   ],
   credentials: true, // allow cookies to be sent
 };
