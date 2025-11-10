71eb9f9f7c504d872d508f49d710faa3650f1d43
supakkit <87434100+supakkit@users.noreply.github.com>
2025-11-10 14:26:32 +0700

    refactor: move frontend url for allowing cors to .env file

--- src/middleware/corsOptions.ts
+++ src/middleware/corsOptions.ts
@@ -2,7 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://notes-app-lime-eta.vercel.app",
+    process.env.FRONTEND_URL,
   ],
   credentials: true, // allow cookies to be sent
 };
